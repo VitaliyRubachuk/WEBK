1b0c3aba8ecb49b9d87be0f0e867325f2caf1b48
Vitaliy Rubachuk <rubachuk288@gmail.com>
2024-06-13 17:27:52 +0300

    Done2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const saltRounds = 10; // кількість раундів для генерації солі
 const app = express();
