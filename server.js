8abb3cf9e554077544687ce1ae42dfa88f52deed
Vitaliy Rubachuk <rubachuk288@gmail.com>
2024-06-13 19:20:51 +0300

    4

--- server.js
+++ server.js
@@ -12,12 +12,11 @@ app.use(cors());
 app.use(bodyParser.json());
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'mysql'
 });
-
 
 db.connect(err => {
     if (err) throw err;
